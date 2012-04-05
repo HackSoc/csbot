26ae5a7a38b2aa293f32f46d378f4859b52c3784
Harry Mills <harry@haeg.in>
2012-04-05 22:31:01 +0100

    Adds a theoretical function to send messages to a user when they join.

--- csbot/plugins/tell.py
+++ csbot/plugins/tell.py
@@ -35,15 +35,18 @@ class Tell(Plugin):
         from_user = event.user
         time = localtime()  # TODO: this should probably do some i18n but
                             # being as the channel is largely in the UK...
+        user_is_here = False
         if (user_is_here):
-                # TODO: implement this
-        msg = {'message': message, 'from': from_user, 'time': time}
-        if (messages.has_key(to_user)):
-            messages[to_user].append(msg)
+            # TODO: implement the check
+            event.reply("{}, {} is here, you can tell them yourself.".format(from_user, to_user))
         else:
-            messages[to_user] = [msg]
+            msg = {'message': message, 'from': from_user, 'time': time}
+            if (messages.has_key(to_user)):
+                messages[to_user].append(msg)
+            else:
+                messages[to_user] = [msg]
 
-        event.reply("{}, I'll let {} know.".format(from_user, to_user))
+            event.reply("{}, I'll let {} know.".format(from_user, to_user))
 
         #event.reply(('test invoked: {0.user}, {0.channel}, '
         #             '{0.data}').format(event))
@@ -62,6 +65,26 @@ class Tell(Plugin):
         > Bot       [13:37] | You have no messages. Sorry.
         """
         # TODO: implement
+
+    def on_join(event):
+        if (hasMessages(event.user)):
+            messages = getMessages(event.user)
+            if (len(messages) <= 1):
+                # If there is only one message we can tell them in the channel
+                sendMessage(event, messages[0]
+            for msg : getMessages(user):
+                # If there is more than one message we need to PM them and tell
+                # to check their PM.
+                sendMessage(event, msg, isPM=True)
+            event.reply("{}, several people left messages for you. Please check the PM I sent you.".format(event.user))
+
+    def sendMessage(event, message, isPM=False):
+        from_user = message['from']
+        to_user = message['to']
+        message = message['message']
+        time = message['time']
+        event.reply("{}, \"{}\" - {} (at {})".format(to_user, message, from_user, time)
+
 
     def getMessages(self, user):
         if (messages.has_key(user)):
