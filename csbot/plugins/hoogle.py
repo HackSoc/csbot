898b5f511b3742771e892d82a1a48532e18d816e
Michael Walker <mike@barrucadu.co.uk>
2012-09-18 00:37:09 +0100

    Add Hoogle plugin
    This commit adds a plugin to search Hoogle, the Haskell search engine. It adds
    one command, of the form "!hoogle query". The query may contain spaces. There
    is one configuration option, "results" which is an integer value specifying
    the number of results to return. This defaults to 5.

---
+++ csbot/plugins/hoogle.py
@@ -0,0 +1,63 @@
+import json
+import logging
+import requests
+import urllib
+
+from csbot.plugin import Plugin
+
+
+class Hoogle(Plugin):
+    log = logging.getLogger(__name__)
+
+    def setup(self):
+        super(Hoogle, self).setup()
+
+    @Plugin.command('hoogle')
+    def search_hoogle(self, e):
+        """Search Hoogle with a given string and return the first few
+        (exact number configurable) results.
+        """
+
+        query = e['data']
+        hoogleurl = 'http://www.haskell.org/hoogle/?mode=json&hoogle=' + query
+        hoogleresp = requests.get(hoogleurl)
+
+        if hoogleresp.status_code != requests.codes.ok:
+            self.log.warn(u'request failed for ' + hoogleurl)
+            return
+
+        # The Hoogle response JSON is of the following format:
+        # {
+        #  "version": "<hoogle version>"
+        #  "results": [
+        #    {
+        #      "location": "<link to docs>"
+        #      "self":     "<name> :: <type>"
+        #      "docs":     "<short description>"
+        #    },
+        #    ...
+        #  ]
+        # }
+
+        maxresults = 5
+
+        try:
+            maxresults = int(self.config_get('results'))
+        except KeyError:
+            self.log.debug(u'number of results not specified, using 5')
+        except ValueError:
+            self.log.warn(u'"results" is not an integer!')
+
+        try:
+            allresults = json.loads(hoogleresp.text)[u'results']
+            totalresults = len(allresults)
+            results = allresults[0:maxresults]
+
+            e.protocol.msg(e['reply_to'], u'Showing {} of {} results:'.format(
+                maxresults if maxresults < totalresults else totalresults,
+                totalresults))
+
+            for result in results:
+                e.protocol.msg(e['reply_to'], result[u'self'])
+        except ValueError:
+            self.log.warn(u'invalid JSON received from Hoogle')
