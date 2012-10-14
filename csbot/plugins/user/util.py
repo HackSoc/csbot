a1dc1a7a6fd254570b5a45f967facb42a4942ab8
Harry Mills <harry@haeg.in>
2012-10-14 01:08:09 +0100

    Hopefully should be the single file users plugin with user class

--- csbot/plugins/user/util.py
+++
@@ -1,8 +0,0 @@
-def needs_db(fn):
-    def decorator():
-        if hasattr(User, 'db'):
-            fn()
-        else:
-            raise DatabaseNotSet("You need to set up the database before calling {}".format(fn.__name__))
-    return decorator
-
