c4318e2c30c753d184d96cb68b316e02b91d7b1e
Harry Mills <harry@freeagent.com>
2012-11-28 16:05:43 +0000

    Removes check for users in tags.py

--- csbot/plugins/tags.py
+++ csbot/plugins/tags.py
@@ -13,18 +13,15 @@ class Tags(Plugin):
     @Plugin.command('play')
     def play(self, event):
         game = event.arguments()[0]
-        if 'users' in event.bot.plugins:
-            requestor = nick(event['user'])
-            users = event.bot.plugins['users'].find_users_by_tag(game)
-            nicks = map(str, users)
-            if requestor in nicks:
-                nicks.remove(requestor)
-            if len(nicks) > 0:
-                message = ", ".join(nicks)
-                message += ": {} wants to play {}".format(requestor, game.title())
-                event.reply(message)
-            else:
-                event.reply("Sorry, nobody else wants to play {}.".format(game.title()))
+        requestor = nick(event['user'])
+        users = event.bot.plugins['users'].find_users_by_tag(game)
+        nicks = map(str, users)
+        if requestor in nicks:
+            nicks.remove(requestor)
+        if len(nicks) > 0:
+            message = ", ".join(nicks)
+            message += ": {} wants to play {}".format(requestor, game.title())
+            event.reply(message)
         else:
-            event.reply("Sorry, without the users plugin loaded I'm useless.")
+            event.reply("Sorry, nobody else wants to play {}.".format(game.title()))
 
