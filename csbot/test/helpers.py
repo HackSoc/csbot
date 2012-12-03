70dc61a598c37e3e4eca7178249debdfa41474b8
Alan Briolat <alan.briolat@gmail.com>
2012-12-03 21:38:27 +0000

    Revert "Add MethodRecorderMixin for testing, and tests for it"
    This reverts commit 85a5e457a7f2527da8f0acfb4fc1e1445ff037cd.
    
    Conflicts:
    	csbot/test/helpers.py

--- csbot/test/helpers.py
+++ csbot/test/helpers.py
@@ -1,31 +1,6 @@
 import os
 
 
-class MethodRecorderMixin(object):
-    """A mixin that records every method call made.
-
-    In this case, a "method" is any callable object attribute, excluding
-    ``__init__``.  Other attributes should remain untouched, including static
-    and class methods.
-    """
-    def __init__(self, *args, **kwargs):
-        super(MethodRecorderMixin, self).__init__(*args, **kwargs)
-        self.recorded_method_calls = []
-
-    def __getattribute__(self, name):
-        # Will raise AttributeError as normal if the attribute is missing
-        attr = super(MethodRecorderMixin, self).__getattribute__(name)
-
-        # Wrap callable attributes in a function that records the method call
-        if callable(attr):
-            def f(*args, **kwargs):
-                self.recorded_method_calls.append((name, args, kwargs))
-                return attr(*args, **kwargs)
-            return f
-        else:
-            return attr
-
-
 class TempEnvVars(object):
     """A context manager for temporarily changing the values of enviroment
     variables."""
