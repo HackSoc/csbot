d060d61f5c6de7cbce9d2fb2cd30e0886c3452fb
Michael Walker <mike@barrucadu.co.uk>
2014-06-15 23:24:29 +0100

    Make whois account-based, falling back to nick-based if not authed.

--- csbot/plugins/whois.py
+++ csbot/plugins/whois.py
@@ -15,8 +15,8 @@ class Whois(Plugin):
         themselves (or an error message if there is no data)"""
 
         nick = e['data'] or nick(e['user'])
-        user = self.whoisdb.find_one({'nick': nick,
-                                      'channel': e['channel']})
+        ident = self.identify_user(nick, e['channel'])
+        user = self.whoisdb.find_one(ident)
 
         if user is None:
             e.protocol.msg(e['reply_to'], u'No data for {}'.format(nick))
@@ -27,8 +27,21 @@ class Whois(Plugin):
     def set(self, e):
         """Allow a user to associate data with themselves for this channel."""
 
-        self.whoisdb.remove({'nick': nick(e['user']),
-                             'channel': e['channel']})
-        self.whoisdb.insert({'nick': nick(e['user']),
-                             'channel': e['channel'],
-                             'data': e['data']})
+        ident = self.identify_user(nick(e['user']), e['channel'])
+        self.whoisdb.remove(ident)
+
+        ident['data'] = e['data']
+        self.whoisdb.insert(ident)
+
+    def identify_user(self, nick, channel):
+        """Identify a user: by account if authed, if not, by nick. Produces a dict
+        suitable for throwing at mongo."""
+
+        user = self.bot.plugins['usertrack'].get_user(nick)
+
+        if user['account'] is not None:
+            return {'account': user['account'],
+                    'channel': channel}
+        else:
+            return {'nick': nick,
+                    'channel': channel}
