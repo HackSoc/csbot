53d4df5611094e1d037e6fcb4003a2d520c73e67
Lord Aro <charlespigott@googlemail.com>
2014-07-22 18:16:52 +0100

    Add xkcd plugin

---
+++ csbot/plugins/xkcd.py
@@ -0,0 +1,94 @@
+import json
+import random
+import requests
+
+from csbot.plugin import Plugin
+from csbot.util import simple_http_get, cap_string, is_ascii
+
+
+def fix_json_unicode(data):
+    """Fixes the unicode silliness that is included in the json data.
+    Why Randall, Why?"""
+    for tag in data:
+        if type(data[tag]) != str or is_ascii(data[tag]):
+            continue
+        try:
+            # Yes, this may need to be done more than once -_-
+            for _ in range(1, 5):
+                data[tag] = data[tag].encode("latin-1").decode("utf-8")
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            pass
+    return data
+
+
+def get_info(number=None):
+    """Gets the json data for a particular comic
+    (or the latest, if none provided).
+    """
+    if number:
+        url = "http://xkcd.com/{}/info.0.json".format(number)
+    else:
+        url = "http://xkcd.com/info.0.json"
+
+    data = simple_http_get(url)
+    if data.status_code != requests.codes.ok:
+        return None
+
+    data = json.loads(data.text)
+
+    # Unfuck up unicode strings
+    data = fix_json_unicode(data)
+
+    data["url"] = "http://xkcd.com/" + str(data["num"])
+    return data
+
+
+class xkcd(Plugin):
+    """A plugin that does some xkcd things.
+    Based on williebot xkcd plugin.
+    """
+
+    def _xkcd(self, user_str):
+        """Get the url and title stuff.
+        Returns a string of the response.
+        """
+
+        latest = get_info()
+        if not latest:
+            return "Error getting comics"
+
+        latest_num = latest["num"]
+
+        if not user_str:
+            requested = latest
+
+        elif user_str.isdigit():
+            num = int(user_str)
+            if num > latest_num:
+                return ("Comic #{} is invalid. "
+                        "The latest is #{}").format(num, latest_num)
+            elif num == 0:
+                requested = latest
+            else:
+                requested = get_info(num)
+
+        elif user_str.lower() in ["rand", "random"]:
+            requested = get_info(random.randint(1, latest_num))
+
+        elif user_str.lower() in ["latest", "current", "newest"]:
+            requested = latest
+        else:
+            # Todo: google search?
+            return "Invalid comic number"
+
+        # Only happens for invalid comics (like 404)
+        if not requested:
+            return "So. It has come to this"
+        return "{} [{} - \"{}\"]".format(requested["url"], requested["title"],
+                                         cap_string(requested["alt"], 120))
+
+    @Plugin.command('xkcd')
+    def randall_is_awesome(self, e):
+        """Well, Randall sucks at unicode actually :(
+        """
+        e.protocol.msg(e["reply_to"], self._xkcd(e["data"]))
