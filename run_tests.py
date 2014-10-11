6d485297be671c9cc403e39e1e22b9b6ccb688e8
Alan Briolat <alan.briolat@gmail.com>
2014-10-11 14:48:34 +0100

    Fix run_tests.py to exit with non-0 status on test failure

--- run_tests.py
+++ run_tests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import unittest
+import sys
 
 suite = unittest.TestLoader().discover('csbot/test', top_level_dir='.')
-unittest.TextTestRunner().run(suite)
+result = unittest.TextTestRunner().run(suite)
+sys.exit(0 if result.wasSuccessful() else 1)
