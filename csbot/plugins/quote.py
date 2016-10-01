2d706b49b7c7da828d79343d2d6fa0de8917851e
johnguy94 <john.barra@hotmail.co.uk>
2016-10-01 23:38:41 +0100

    Moved to MongoDB
    add quote now uses last message by person, get quote now uses nick

--- csbot/plugins/quote.py
+++ csbot/plugins/quote.py
@@ -1,95 +1,79 @@
-import json
 import last
+import random
 from ..plugin import Plugin
 
 class Quote(Plugin):
     """Quote people out of context for fun and for profit.
        Add quotes, recall quotes
     """
+    db = Plugin.use('mongodb', collection='quote')
+
     class QuoteError(Exception):
         pass
 
-    def _quote(self, in_quote_num=None):
-        quote = {}
-        if in_quote_num == None:
-            raise self.QuoteError("No quote number passed.")
-        #check format of quote_numb
-        quote["ID"] = 0
-        try:
-            quote["ID"] = int(in_quote_num)
-        except:
-            raise self.QuoteError("Quote number passed not integer.")
+    def _quote(self, nick):
+        """Find a quote by person"""
+        if not nick:
+            self.QuoteError("No nick!")
 
-        #open the quotes file, and read the quote
-        quotes = {}
-        try:
-            with open("quotes.json","r") as q_file:
-                try:
-                    quotes = json.loads(q_file.read())
-                except:
-                    raise self.QuoteError("Quotes JSON file format error.")
-        except:
-            raise self.QuoteError("Quote file doesn't exist yo.")
+        #find quote by nick
+        quotes = self.db.find({'nick': nick})
+        quote_list = list(quotes)
 
-        #get quote
-        quote["text"] = ""
-        try:
-            quote["text"] = quotes[str(in_quote_num)]
-        except:
-            return (quote["ID"], "NOT FOUND")
+        if len(quote_list) == 0:
+            self.QuoteError("No quotes by", nick)
 
-        return (quote["ID"], quote["text"])
+        #Randomly pick quote from returned quotes
+        quote_list = list(quotes)
+        quote = random.choice(quote_list)
 
-    def _addquote(self, in_quote_text=""):
-        if in_quote_text == "":
-            raise self.QuoteError("Empty quote!")
+        return (quote["text"], quote["nick"])
+
+    def _quoteExists(self, quote_post):
+        """Checks to see if a nick + quote already exists to stop repeats."""
+        if self.db.find(quote_post).count > 0:
+            return True
+        else:
+            return False
+
+    def _addquote(self, nick=""):
+        """Add quote from what person last said"""
+
+        if not nick:
+            raise self.QuoteError("No nick!")
+
         quote = {} #output
+        quote["quote"] = last.last_message(nick)[2]
 
-        #open up quotes file
-        quotes = {}
-        try:
-            with open("quotes.json","r") as q_file:
-                quotes = json.loads(q_file.read())
-        except:
-            #quote file doesn't exist
-            quotes = {"quote_count": 1, '0': "Test quote please ignore."}
+        if not quote["quote"]:
+            raise self.QuoteError("No Last message from nick, or nick not found.")
 
-        #read the last key used, , this becomes the quote id, add one to it
-        num_of_quotes = 0
-        try:
-            num_of_quotes = quotes["quote_count"]
-        except:
-            raise self.QuoteError("Can't find quote count in quote dictionary.")
-        quotes[num_of_quotes] = in_quote_text
-        quote["ID"] = num_of_quotes
-        quotes["quote_count"] = num_of_quotes + 1
+        #Create post object
+        post_quote = {'nick': nick, 'quote': quote["quote"]}
 
-        #write quote file back
-        try:
-            with open("quotes.json","w+") as q_file:
-                q_file.write(json.dumps(quotes))
-        except:
-            raise self.QuoteError("Unable to write quote file back")
+        #check if quote already in database
+        if not _quoteExists(post_quote):
+            self.db.insert_one(post_quote)
+        else:
+            self.QuoteError("Quote already in Database.")
 
-        quote["text"] = in_quote_text
-
-        return(quote["text"], quote["ID"])
+        return(quote["quote"], nick)
 
 
-    @Plugin.command('quote', help="quote <ID>")
+    @Plugin.command('quote', help="quote [nick]")
     def quote(self, e):
         """quote somebody
         """
         try:
-            e.reply("#{}: {}".format(*self._quote(e["data"])))
+            e.reply("\"{}\" - {}".format(*self._quote(e["data"])))
         except self.QuoteError as ex:
             e.reply(str(ex))
 
-    @Plugin.command('addquote', help="addquote <quote>")
+    @Plugin.command('addquote', help="addquote <nick>")
     def addquote(self, e):
         """add a quote
         """
         try:
-            e.reply("\"{}\" added as quote #{}.".format(*self._addquote(e["data"])))
+            e.reply("\"{}\" - {} added as quote.".format(*self._addquote(e["data"])))
         except self.QuoteError as ex:
             e.reply(str(ex))
