9c40cb1feb67e1d031c9d72352ff64f7c98d06e8
Ben Simner <benjsimner@gmail.com>
2017-03-22 19:30:04 +0000

    Decouple whois implementation from client events
    This makes it (slightly) easier to test. Coincidentally this commit also includes more tests.

--- csbot/plugins/whois.py
+++ csbot/plugins/whois.py
@@ -10,30 +10,44 @@ class Whois(Plugin):
 
     whoisdb = Plugin.use('mongodb', collection='whois')
 
+    def whois_lookup(self, nick, channel, db=None):
+        """Performs a whois lookup for a nick on a specific channel"""
+        db = db or self.whoisdb
+
+        ident = self.identify_user(nick, channel)
+        user = db.find_one(ident)
+
+        if user is None:
+            return None
+        else:
+            return user['data']
+
+    def whois_set(self, nick, channel, whois_str, db=None):
+        db = db or self.whoisdb
+
+        ident = self.identify_user(nick, channel)
+        db.remove(ident)
+
+        ident['data'] = whois_str
+        db.insert(ident)
+
     @Plugin.command('whois', help=('whois [nick]: show whois data for'
                                    ' a nick, or for yourself if omitted'))
     def whois(self, e):
         """Look up a user by nick, and return what data they have set for
         themselves (or an error message if there is no data)"""
-
         nick_ = e['data'] or nick(e['user'])
-        ident = self.identify_user(nick_, e['channel'])
-        user = self.whoisdb.find_one(ident)
+        res = self.whois_lookup(nick_, e['channel'])
 
-        if user is None:
+        if res is None:
             e.reply('No data for {}'.format(nick_))
         else:
-            e.reply('{}: {}'.format(nick_, user['data']))
+            e.reply('{}: {}'.format(nick_, str(res)))
 
     @Plugin.command('whois.set')
     def set(self, e):
         """Allow a user to associate data with themselves for this channel."""
-
-        ident = self.identify_user(nick(e['user']), e['channel'])
-        self.whoisdb.remove(ident)
-
-        ident['data'] = e['data']
-        self.whoisdb.insert(ident)
+        self.whois_set(nick(e['user']), e['channel'], e['data'])
 
     def identify_user(self, nick, channel):
         """Identify a user: by account if authed, if not, by nick. Produces a dict
--- csbot/test/test_plugin_whois.py
+++ csbot/test/test_plugin_whois.py
@@ -20,13 +20,33 @@ class TestWhoisPlugin(BotTestCase):
         collection = MongoClient().db.collection
         self.whois.whoisdb = collection
 
+    def test_whois_empty(self):
+        assert self.whois.whois_lookup('this_nick_doesnt_exist', '#anyChannel') is None
+
+    def test_whois_insert(self):
+        self.whois.whois_set('nick1', '#channel1', 'test data')
+        assert self.whois.whois_lookup('nick1', '#channel1') == 'test data'
+        assert self.whois.whois_lookup('nick1', '#otherChannel') is None
+
+    def test_whois_multi_user(self):
+        self.whois.whois_set('nick1', '#channel1', 'test1')
+        self.whois.whois_set('nick2', '#channel1', 'test2')
+        assert self.whois.whois_lookup('nick1', '#channel1') == 'test1'
+        assert self.whois.whois_lookup('nick2', '#channel1') == 'test2'
+
+    def test_whois_multi_channel(self):
+        self.whois.whois_set('nick1', '#channel1', 'test data1')
+        self.whois.whois_set('nick1', '#channel2', 'test data2')
+        assert self.whois.whois_lookup('nick1', '#channel1') == 'test data1'
+        assert self.whois.whois_lookup('nick1', '#channel2') == 'test data2'
+
     @run_client
-    def test_whois_set_singlechannel(self):
+    def test_client_set_single_channel(self):
         yield from self._recv_privmsg('Nick!~user@host', '#channel', '!whois.set test')
         self._assert_whois('Nick', '#channel', 'test')
 
     @run_client
-    def test_whois_set_multichannel(self):
+    def test_client_set_multi_channel(self):
         yield from self._recv_privmsg('Nick!~user@host', '#channel1', '!whois.set test1')
         yield from self._recv_privmsg('Nick!~user@host', '#channel2', '!whois.set test2')
 
