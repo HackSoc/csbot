7e8fc82c83e54cdf2c8ba3533ffb212461f3fb78
Ben Simner <benjsimner@gmail.com>
2017-03-22 20:21:40 +0000

    Assert replies for whois tests
    Adds a few tests asserting the desired reply of the bot for trivial !whois and !whois.set interleavings.

--- csbot/test/test_plugin_whois.py
+++ csbot/test/test_plugin_whois.py
@@ -24,21 +24,21 @@ class TestWhoisPlugin(BotTestCase):
         assert self.whois.whois_lookup('this_nick_doesnt_exist', '#anyChannel') is None
 
     def test_whois_insert(self):
-        self.whois.whois_set('nick1', '#channel1', 'test data')
-        assert self.whois.whois_lookup('nick1', '#channel1') == 'test data'
+        self.whois.whois_set('nick1', '#First', 'test data')
+        assert self.whois.whois_lookup('nick1', '#First') == 'test data'
         assert self.whois.whois_lookup('nick1', '#otherChannel') is None
 
     def test_whois_multi_user(self):
-        self.whois.whois_set('nick1', '#channel1', 'test1')
-        self.whois.whois_set('nick2', '#channel1', 'test2')
-        assert self.whois.whois_lookup('nick1', '#channel1') == 'test1'
-        assert self.whois.whois_lookup('nick2', '#channel1') == 'test2'
+        self.whois.whois_set('nick1', '#First', 'test1')
+        self.whois.whois_set('nick2', '#First', 'test2')
+        assert self.whois.whois_lookup('nick1', '#First') == 'test1'
+        assert self.whois.whois_lookup('nick2', '#First') == 'test2'
 
     def test_whois_multi_channel(self):
-        self.whois.whois_set('nick1', '#channel1', 'test data1')
-        self.whois.whois_set('nick1', '#channel2', 'test data2')
-        assert self.whois.whois_lookup('nick1', '#channel1') == 'test data1'
-        assert self.whois.whois_lookup('nick1', '#channel2') == 'test data2'
+        self.whois.whois_set('nick1', '#First', 'test data1')
+        self.whois.whois_set('nick1', '#Second', 'test data2')
+        assert self.whois.whois_lookup('nick1', '#First') == 'test data1'
+        assert self.whois.whois_lookup('nick1', '#Second') == 'test data2'
 
     @run_client
     def test_client_set_single_channel(self):
@@ -47,8 +47,31 @@ class TestWhoisPlugin(BotTestCase):
 
     @run_client
     def test_client_set_multi_channel(self):
-        yield from self._recv_privmsg('Nick!~user@host', '#channel1', '!whois.set test1')
-        yield from self._recv_privmsg('Nick!~user@host', '#channel2', '!whois.set test2')
+        yield from self._recv_privmsg('Nick!~user@host', '#First', '!whois.set test1')
+        yield from self._recv_privmsg('Nick!~user@host', '#Second', '!whois.set test2')
 
-        self._assert_whois('Nick', '#channel1', 'test1')
-        self._assert_whois('Nick', '#channel2', 'test2')
+        self._assert_whois('Nick', '#First', 'test1')
+        self._assert_whois('Nick', '#Second', 'test2')
+
+    @run_client
+    def test_client_reply_whois_after_set(self):
+        yield from self._recv_privmsg('Nick!~user@host', '#First', '!whois.set test1')
+        yield from self._recv_privmsg('Nick!~user@host', '#First', '!whois')
+        self.assert_sent('NOTICE {} :{}'.format('#First', 'Nick: test1'))
+
+    @run_client
+    def test_client_reply_whois_different_channel(self):
+        yield from self._recv_privmsg('Nick!~user@host', '#First', '!whois.set test1')
+        yield from self._recv_privmsg('Nick!~user@host', '#Second', '!whois')
+        self.assert_sent('NOTICE {} :{}'.format('#Second', 'No data for Nick'))
+
+    @run_client
+    def test_client_reply_whois_multiple_users_channels(self):
+        yield from self._recv_privmsg('Nick!~user@host', '#First', '!whois.set test1')
+        yield from self._recv_privmsg('Nick!~user@host', '#Second', '!whois.set test2')
+
+        yield from self._recv_privmsg('Other!~user@host', '#First', '!whois Nick')
+        self.assert_sent('NOTICE {} :{}'.format('#First', 'Nick: test1'))
+
+        yield from self._recv_privmsg('Other!~user@host', '#Second', '!whois Nick')
+        self.assert_sent('NOTICE {} :{}'.format('#Second', 'Nick: test2'))
