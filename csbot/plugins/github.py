b3ac3de7eecbfe6e798279b0d0ea4fdf0052bceb
Alan Briolat <alan.briolat@gmail.com>
2019-01-25 22:22:45 +0000

    WIP: github webhook

--- csbot/plugins/github.py
+++ csbot/plugins/github.py
@@ -1,3 +1,5 @@
+import hmac
+
 from ..plugin import Plugin
 
 
@@ -9,10 +11,24 @@ class GitHub(Plugin):
         self.log.info("Handling github webhook")
         request = e['request']
         github_event = request.headers['X-GitHub-Event'].lower()
+        digest = request.headers['X-GitHub-Signature']
+        payload = await request.read()
+        if not self._hmac_compare(payload, digest):
+            self.log.warning('X-GitHub-Signature verification failed')
+            return
         method = getattr(self, f'handle_{github_event}', None)
         if method is not None:
-            await method(request)
+            await method(await request.json())
 
-    async def handle_ping(self, request):
-        data = await request.json()
+    async def handle_ping(self, data):
         self.log.info("Received ping event: {}", data)
+
+    def _hmac(self, msg):
+        secret = self.bot.plugins['webhook'].config_get('secret').encode('utf-8')
+        return hmac.new(secret, msg, 'sha1')
+
+    def _hmac_digest(self, msg):
+        return self._hmac(msg).hexdigest()
+
+    def _hmac_compare(self, msg, digest):
+        return hmac.compare_digest(self._hmac_digest(msg), digest)
--- csbot/test/test_plugin_github.py
+++ csbot/test/test_plugin_github.py
@@ -1,10 +1,23 @@
-import pytest
+import json
 
+import pytest
+import asynctest
+
+from csbot import core
 from csbot.test import BotTestCase, read_fixture_file
+from csbot.test.test_plugin_webserver import WebServer
+
+
+class Bot(core.Bot):
+    available_plugins = core.Bot.available_plugins.copy()
+    available_plugins.update(
+        webserver=WebServer,
+    )
 
 
 class TestGitHubPlugin(BotTestCase):
     SECRET = 'foobar'
+    BOT_CLASS = Bot
     CONFIG = f"""\
     [@bot]
     plugins = webserver webhook github
@@ -16,6 +29,7 @@ class TestGitHubPlugin(BotTestCase):
     
     """
     PLUGINS = ['webserver', 'github']
+    URL = f'/webhook/github/{SECRET}'
 
     @pytest.fixture
     def loop(self, event_loop):
@@ -23,25 +37,82 @@ class TestGitHubPlugin(BotTestCase):
         """
         return event_loop
 
+    def _payload_and_headers_from_fixture(self, event, filename):
+        payload = read_fixture_file(filename)
+        headers = {
+            'X-GitHub-Event': event,
+            'X-GitHub-Delivery': '00000000-0000-0000-0000-000000000000',
+            'X-GitHub-Signature': self.github._hmac_digest(payload),
+        }
+        return payload, headers
+
+    def _payload_and_headers_from_object(self, event, obj):
+        payload = json.dumps(obj).encode('utf-8')
+        headers = {
+            'X-GitHub-Event': event,
+            'X-GitHub-Delivery': '00000000-0000-0000-0000-000000000000',
+            'X-GitHub-Signature': self.github._hmac_digest(payload),
+        }
+        return payload, headers
+
+    # @pytest.fixture
+    # async def webserver_client(self, aiohttp_client, bot_setup):
+    #     print("doing webserver_client")
+    #     print(self)
+    #     return await aiohttp_client(self.webserver.app)
+
+    async def test_signature_check(self, aiohttp_client):
+        client = await aiohttp_client(self.webserver.app)
+        with asynctest.patch.object(self.github, 'handle_foo', new=asynctest.CoroutineMock(), create=True) as m:
+            payload = {}
+            data = json.dumps(payload).encode('utf-8')
+            headers = {
+                'X-GitHub-Event': 'foo',
+                'X-GitHub-Delivery': '00000000-0000-0000-0000-000000000000',
+                'X-GitHub-Signature': '0000000000000000000000000000000000000000',
+            }
+            resp = await client.post(self.URL, data=data, headers=headers)
+            assert resp.status == 200
+            m.assert_not_called()
+
+            headers['X-GitHub-Signature'] = self.github._hmac_digest(data)
+            resp = await client.post(self.URL, data=data, headers=headers)
+            assert resp.status == 200
+            m.assert_called_once_with(payload)
+
     async def test_ping(self, aiohttp_client):
         # https://developer.github.com/webhooks/#ping-event
-        payload = """\
-        {
-          "hook":{
-            "type":"App",
-            "id":11,
-            "active":true,
-            "events":["pull_request"],
-            "app_id":37
-          }
-        }
-        """.encode("utf-8")
+        payload, headers = self._payload_and_headers_from_object("ping", {
+            "hook": {
+                "type": "App",
+                "id": 11,
+                "active": True,
+                "events": ["pull_request"],
+                "app_id": 37
+            }
+        })
 
         client = await aiohttp_client(self.webserver.app)
-        resp = await client.post(f'/webhook/github/{self.SECRET}', data=payload)
-        assert resp.status == 200
+        with asynctest.patch.object(self.github, 'handle_ping') as m:
+            resp = await client.post(
+                self.URL,
+                data=payload,
+                headers={
+                    'X-GitHub-Event': 'ping',
+                    'X-GitHub-Delivery': '00000000-0000-0000-0000-000000000000',
+                    'X-GitHub-Signature': self.github._hmac_digest(payload),
+                },
+            )
+            assert resp.status == 200
+            m.assert_called_once()
+    #
+    # @pytest.mark.usefixtures("run_client")
+    # async def test_push(self, aiohttp_client):
+    #     payload, headers = self._payload_and_headers_from_fixture("push", 'github_webhook_push.json')
+    #     client = await aiohttp_client(self.webserver.app)
+    #     resp = await client.post(self.URL, data=payload, headers=headers)
+    #     self.assert_sent([''])
 
-    async def test_push(self, aiohttp_client):
-        payload = read_fixture_file('github_push.json')
-        client = await aiohttp_client(self.webserver.app)
-        resp = await client.post()
+    # @pytest.mark.usefixtures("run_client")
+    # async def test_issue_opened(self, webserver_client):
+    #     pass
