c597818230e7db38d8b0ce849c7cbc96b67fdb6d
Alan Briolat <alan.briolat@gmail.com>
2019-06-07 16:43:57 +0100

    Move runtime requirements to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,20 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
+    install_requires=[
+        'click>=6.2,<7.0',
+        'straight.plugin==1.4.0-post-1',
+        'pymongo>=3.6.0',
+        'requests>=2.9.1,<3.0.0',
+        'lxml>=2.3.5',
+        'aiogoogle>=0.1.13',
+        'isodate>=0.5.1',
+        'aiohttp>=3.5.1,<4.0',
+        'async_generator',
+        'rollbar',
+    ],
+    extras_require={
+    },
     entry_points={
         'console_scripts': [
             'csbot = csbot:main',
